80dc7cc79f02d2fa6506d425b2ba9c307abc29ff
kao.ring <kao.ring@yahoo.com>
2020-09-14 14:28:53 -0400

    added chart

--- client/src/pages/Progress.js
+++ client/src/pages/Progress.js
@@ -1,100 +1,46 @@
 import React from "react";
-// import { Line } from "react-chartjs-2";
-// import moment from "moment";
-// import API from "../utils/API";
+import CanvasJSReact from "./assets/canvasjs.react";
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+// var CanvasJS = CanvasJSReact.CanvasJS;
 
-export default class Progress extends React.Component {
-  // state = {
-  //   labels: [],
-  //   score: [],
-  //   tests: [],
-  //   user: "",
-  //   // labels: this.state.labels,
-  // };
+function Progress() {
+  const options = {
+    animationEnabled: true,
+    theme: "light2",
 
-  // componentDidMount() {
-  //   const startDate = new Date(2020, 8, 1);
-  //   let labels = [];
-  //   let score = [];
-  //   let tests = [];
+    axisX: {
+      title: "User Name",
+      reversed: true,
+    },
+    axisY: {
+      title: "Total Score",
+    },
+    data: [
+      {
+        type: "bar",
+        dataPoints: [
+          { y: 500, label: "Kiki" },
+          { y: 460, label: "Alisa" },
+          { y: 300, label: "Jake" },
+          { y: 280, label: "Gabriel" },
+          { y: 100, label: "Kaori" },
+          { y: 10, label: "Asd" },
+          { y: 0, label: "Qwe" },
+        ],
+      },
+    ],
+  };
 
-  //   for (let i = 0; i < 30; i++) {
-  //     const date = moment(startDate).add(i, "days").format("YYYY-MM-DD");
-  //     labels.push(date.toString());
-  //     score.push(Math.floor(Math.random() * 30)); //仮データ
-  //     tests.push(Math.floor(Math.random() * 10)); //仮データ
-  //   }
-  //   let user = JSON.parse(localStorage.getItem("authUser")).user_name;
-  //   this.setState({ labels: labels, score: score, tests: tests });
-
-  //   if (user) {
-  //     this.setState({ user: user });
-  //   } else {
-  //     this.getUser(user);
-  //   }
-  // }
-
-  // getUser(user) {
-  //   API.getUserInfo(user)
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       this.totalScore(res.data.tests);
-  //       this.numberOfTests(res.data.tests);
-  //     })
-  //     .catch((err) => console.log(err));
-
-  //   API.getAllUserInfo()
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }
-
-  // totalScore(data) {
-  //   console.log(data);
-  // }
-
-  // numberOfTests(data) {
-  //   console.log(data);
-  // }
-
-  // data = (canvas) => {
-  //   // const ctx = canvas.getContext("2d");
-  //   // const gradient = ctx.createLinearGradient(0, 0, 100, 0);
-  //   return {
-  //     backgroundColor: "red",
-  //     // labels,
-  //     datasets: [
-  //       {
-  //         label: this.state.labels,
-  //         data: this.state.score,
-  //         borderWidth: 3,
-  //         fill: false,
-  //         borderColor: "green",
-  //       },
-  //     ],
-  //   };
-  // };
-
-  render() {
-    return (
-      <div>
-        {/* <Line
-          data={this.data}
-          options={{
-            title: {
-              display: true,
-              text: this.state.user + "'s Daily Progress",
-              fontSize: 20,
-            },
-            legend: {
-              display: true,
-              position: "right",
-            },
-          }}
-        /> */}
-        Progress page
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Ranking</h1>
+      <CanvasJSChart
+        options={options}
+        /* onRef={ref => this.chart = ref} */
+      />
+      {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+    </div>
+  );
 }
+
+export default Progress;
