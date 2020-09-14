dfb48b08c1cbde91bddec8280759e4b44dee2136
kao.ring <kao.ring@yahoo.com>
2020-09-14 15:52:51 -0400

    added labels to chart

--- client/src/pages/Progress.js
+++ client/src/pages/Progress.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CanvasJSReact from "./assets/canvasjs.react";
+import API from "../utils/API";
+
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 // var CanvasJS = CanvasJSReact.CanvasJS;
 
 function Progress() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userInfo();
+    // console.log("エフェクト");
+    // console.log(users);
+  }, []);
+
+  const userInfo = () => {
+    API.getAllUserInfo()
+      .then((res) => {
+        console.log("ログデータ");
+        console.log(res.data);
+
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // const scoreTotal = (tests) => {
+  //   tests.map((test) => {
+  //     let total = 0;
+  //     total += test.score;
+  //     return 100;
+  //   });
+  // };
+
   const options = {
     animationEnabled: true,
     theme: "light2",
 
     axisX: {
-      title: "User Name",
+      title: "Student's Name",
       reversed: true,
     },
     axisY: {
@@ -18,22 +47,26 @@ function Progress() {
     data: [
       {
         type: "bar",
-        dataPoints: [
-          { y: 500, label: "Kiki" },
-          { y: 460, label: "Alisa" },
-          { y: 300, label: "Jake" },
-          { y: 280, label: "Gabriel" },
-          { y: 100, label: "Kaori" },
-          { y: 10, label: "Asd" },
-          { y: 0, label: "Qwe" },
-        ],
+        dataPoints: users.map((user) => {
+          // return { y: scoreTotal(user.tests), label: user.user_name };
+          return { y: Math.floor(Math.random() * 100), label: user.user_name };
+        }),
       },
     ],
   };
 
+  // =====
+  // const scoreArr = user.tests[i].score;
+  // const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  // // 1 + 2 + 3 + 4
+  // console.log(array1.reduce(reducer));
+  // ======
+
   return (
     <div>
-      <h1>Ranking</h1>
+      <h1>Student's Progress</h1>
+
       <CanvasJSChart
         options={options}
         /* onRef={ref => this.chart = ref} */
